c9d5734c913a8b8ada72a12c920c22e49badcb9d
Yunzhi Gao <gaoyunzhi@gmail.com>
2021-03-07 11:42:51 -0800

    commit

--- reddit_collect.py
+++ reddit_collect.py
@@ -6,7 +6,8 @@ import praw
 from telegram_util import log_on_fail
 from telegram.ext import Updater
 import plain_db
-# import reddit_2_album
+import reddit_2_album
+import album_sender
 
 with open('credential') as f:
 	credential = yaml.load(f, Loader=yaml.FullLoader)
@@ -16,15 +17,7 @@ with open('db/setting') as f:
 
 existing = plain_db.loadKeyOnlyDB('existing')
 
-# reddit = reddit_2_album.reddit
-
-reddit = praw.Reddit(
-	client_id=credential['reddit_client_id'],
-	client_secret=credential['reddit_client_secret'],
-	password=credential['reddit_password'],
-	user_agent="testscript",
-	username=credential['reddit_username'],
-)
+reddit = reddit_2_album.reddit
 
 tele = Updater(credential['bot_token'], use_context=True)
 debug_group = tele.bot.get_chat(credential['debug_group'])
@@ -34,17 +27,16 @@ channel = tele.bot.get_chat(credential['channel'])
 def run():
 	for subname in setting['subreddits']:
 		subreddit = reddit.subreddit(subname)
-		for submission in subreddit.hot(limit=10):
+		for submission in subreddit.hot(limit=60):
 			if submission.score < 500:
 				continue
 			if not existing.add(submission.url):
 				continue
 			if submission.permalink != submission.url and not existing.add(submission.permalink):
 				continue
-			url = 'https://www.reddit.com' + submission.permalink
-			print("test('%s')" % url)
-			# album = reddit_2_album.get(url)
-			# album_sender.send_v2(channel, album)
+			url = 'http://www.reddit.com' + submission.permalink
+			album = reddit_2_album.get(url)
+			album_sender.send_v2(channel, album)
 
 if __name__ == '__main__':
 	run()
