72f0f30a1bc423d5bbc6db29044a88fb1abe2525
Yunzhi Gao <gaoyunzhi@gmail.com>
2021-03-06 18:28:14 -0800

    commit

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import os
 import sys
 
 def kill():
-	os.system("ps aux | grep ython | grep douban | awk '{print $2}' | xargs kill -9")
+	os.system("ps aux | grep ython | grep reddit_collect | awk '{print $2}' | xargs kill -9")
 
 def setup():
 	kill()
 	if 'kill' in str(sys.argv):
 		return 
 	addtional_arg = ' '.join(sys.argv[1:])
-	command = 'python3 -u douban.py %s' % addtional_arg
+	command = 'python3 -u reddit_collect.py %s' % addtional_arg
 	if 'debug' in addtional_arg:
 		os.system(command + ' test')
 	else:
